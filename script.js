7f5e45b768cd175929f53cc2070dd5d8e185deb9
Adeyemo Tomisin Stephen <66416030+TomisinAdeyemo@users.noreply.github.com>
2024-06-13 13:07:52 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+/* function hideLoader() {
+    setTimeout(() => {
+        document.getElementById('loader').style.display = 'none';
+        document.getElementById('content').style.display = 'block';
+    }, 2000);
+} */

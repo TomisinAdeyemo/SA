fc7d9a6d9c41f2bf3ab332597c8478ac98c56ff1
Adeyemo Tomisin Stephen <66416030+TomisinAdeyemo@users.noreply.github.com>
2024-08-11 22:55:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,8 @@
         document.getElementById('content').style.display = 'block';
     }, 2000);
 } */
+
+window.addEventListener('load', function() {
+    document.getElementById('loader').style.display = 'none';
+    document.getElementById('content').classList.remove('hidden');
+});
